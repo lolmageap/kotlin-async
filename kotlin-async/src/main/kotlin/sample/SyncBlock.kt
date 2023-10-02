11583e47a8976a48339c02008ff10a3d68611651
cherhy <ekxk1234@naver.com>
2023-10-02 12:06:28 +0900

    sync blocking 예제

---
+++ kotlin-async/src/main/kotlin/sample/SyncBlock.kt
@@ -0,0 +1,25 @@
+package sample
+
+import mu.KotlinLogging
+import kotlin.system.measureTimeMillis
+
+private val logger = KotlinLogging.logger{}
+
+fun main() {
+    measureTimeMillis {
+        subA()
+        subB()
+    }.let { logger.debug { ">> $it ms" } }
+}
+
+private fun subA() {
+    logger.debug { "start A" }
+    Thread.sleep(1000)
+    logger.debug { "end A" }
+}
+
+private fun subB() {
+    logger.debug { "start B" }
+    Thread.sleep(1000)
+    logger.debug { "end B" }
+}
