08dc74c5343448c1de0f566378eb81598984527e
cherhy <ekxk1234@naver.com>
2023-10-06 23:44:01 +0900

    코루틴 의존성 추가

--- kotlin-async/build.gradle.kts
+++ kotlin-async/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("io.projectreactor:reactor-core:3.5.8")
     implementation("ch.qos.logback:logback-classic:1.4.11")
     implementation("io.github.microutils:kotlin-logging:3.0.5")
